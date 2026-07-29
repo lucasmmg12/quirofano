1b2156f2b55559ab36eb82689b09f3e071aa5169
Innovacion y Transformacion Digital <innovacion@sanatorioargentino.com>
2026-07-29 10:11:54 -0300

    fix: corregir etiqueta div extra que rompia el build en Vercel

--- src/components/PublicRecepcionView.jsx
+++ src/components/PublicRecepcionView.jsx
@@ -859,7 +859,6 @@ export default function PublicRecepcionView() {
                                 </div>
                             )}
                         </div>
-                        </div>
                     </div>
                 )}
 
